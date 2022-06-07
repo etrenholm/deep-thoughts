4492f8b5e7258ec3886ba7983bd82e4b9d8aeb09
Erica <erica.trenholm48@gmail.com>
2022-06-06 21:23:01 -0400

    added front end

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -10,15 +12,26 @@ const server = new ApolloServer({
   resolvers
 });
 
+const cors = require('cors')
 const app = express();
+
+app.use(cors()) // enable `cors` to set HTTP response header: Access-Control-Allow-Origin: *
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
- 
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
   
   db.once('open', () => {
     app.listen(PORT, () => {
